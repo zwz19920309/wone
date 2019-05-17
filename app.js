95e9f4e88c532643793af46ba068df517acb7b1e
钟文治 <13824443763@qq.com>
2019-05-17 14:47:40 +0800

    签到配置

--- app.js
+++ app.js
@@ -60,5 +60,5 @@ app.on('error', (err, ctx) => {
   console.error('server error', err, ctx)
 })
 app.listen(process.env.PORT || 80)
-console.log('OT App started at port 8080...')
+console.log('OT App started at port 80...')
 module.exports = app
